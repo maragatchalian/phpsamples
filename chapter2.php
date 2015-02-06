a2db439b2862d063fd1625854f13a472c1aadbc7
mara <maragatchalian@github.com>
2015-02-06 15:47:17 +0800

    Sample Assignments

--- chapter2.php
+++ chapter2.php
@@ -6,7 +6,7 @@ echo "If you take a look at the code, you will see an example of a Function Call
 	#Function Calls
 	#Assignments
 	#If statement
-
+/*
 myFunction(42, "O'Reilly");
 $a = 1;
 $name = "Elphaba";
@@ -15,6 +15,19 @@ $b = $a / 25.0;
 if ($a == $b){
 	echo "Rhyme? And Reason?";
 }
+?>
+*/
+echo "  " ."<br>" ."<br>";
+echo "My Sample Assignments" ."<br>";
+
+$FName = "Mara";
+$Desc = "is cute";
+echo ("Hi! Did you know that $FName, $Desc?" ."<br>" ."<br>");
+
+echo ("Here's the code!:" ."<br>");
+echo '$FName = "Mara";'."<br>";
+echo '$Desc = "is cute";' ."<br>";
+echo 'echo("Hi! Did you know that $FName, $Desc?");"';
 
 
 ?> 

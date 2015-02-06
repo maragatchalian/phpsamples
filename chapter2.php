43a0c6f658e6e8c9802a615fc17f0799dd9713c9
mara <maragatchalian@github.com>
2015-02-06 16:53:32 +0800

    Arrays

--- chapter2.php
+++ chapter2.php
@@ -1,7 +1,8 @@
 <?php
 echo "Hello World" ."<br>";
 echo "If you take a look at the code, you will see an example of a Function Call, Assignments, and an  If Statement." ."<br>";
-
+?>
+<?php
 //Just a Sample of:
 	#Function Calls
 	#Assignments
@@ -15,8 +16,11 @@ $b = $a / 25.0;
 if ($a == $b){
 	echo "Rhyme? And Reason?";
 }
-?>
 */
+?>
+
+<?php
+
 echo "  " ."<br>" ."<br>";
 echo "My Sample Assignments" ."<br>";
 
@@ -29,5 +33,54 @@ echo '$FName = "Mara";'."<br>";
 echo '$Desc = "is cute";' ."<br>";
 echo 'echo("Hi! Did you know that $FName, $Desc?");"';
 
+?>
 
-?> 
+<?php
+echo "  " ."<br>" ."<br>";
+echo "My Sample Associative Array (Better check the codes for details)" ."<br>";
+//Arrays
+	#Associative Array
+	$person[0] = "Mara";
+	$person[1] = "Eric";
+	$person[2] = "Nina";
+	$person[3] = "Dale";
+	
+echo "$person[0] and $person[1]";
+?>
+
+<?php
+//Arays(2)
+	#Associative Array with Looping
+echo "  " ."<br>" ."<br>";
+echo "My Sample Associative Array with Looping using foreach loop" ."<br>";
+	
+	$Person[0] = "Mara";
+	$Person[1] = "Eric";
+	$Person[2] = "Nina";
+	$Person[3] = "Dale";
+
+	#Looping using foreach loop
+	foreach ($Person as $Name){
+		echo "Hello, {$Name}" ."<br>";
+		}
+?>
+
+<?php
+/*
+//Arrays (3)
+	#Array Construct array()
+echo "  " ."<br>" ."<br>" ."<br>"
+echo "My Sample Associative Array using the array() construct";
+
+	$heyyou = array("Mara", "Nina", "Eric", "Dale");
+	$whatnow = array('Candies' => "Mara",
+			'Ribbons' => "Nina",
+			'Games' => "Eric",
+			'Coffee' => "Dale");
+
+	#Using the foreach loop once again
+	foreach ($whatnow as $fave => $whosfaveisit){
+		echo "{$whosfaveisit} likes {$fave}" ."<br>";
+		}	 	
+*/
+?>

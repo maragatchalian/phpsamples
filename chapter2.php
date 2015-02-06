e14e654a6affc985ad668509eb7b53fc8024e8bf
mara <maragatchalian@github.com>
2015-02-06 14:50:44 +0800

    Function Calls, Assignments, and If Else Samples

---
+++ chapter2.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello World" ."<br>";
+echo "If you take a look at the code, you will see an example of a Function Call, Assignments, and an  If Statement." ."<br>";
+
+//Just a Sample of:
+	#Function Calls
+	#Assignments
+	#If statement
+
+myFunction(42, "O'Reilly");
+$a = 1;
+$name = "Elphaba";
+$b = $a / 25.0;
+
+if ($a == $b){
+	echo "Rhyme? And Reason?";
+}
+
+
+?> 

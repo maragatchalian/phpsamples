ac1d634a276de32a8814a63aab535ad32681ce9e
mara <maragatchalian@github.com>
2015-02-06 14:06:12 +0800

    Forms

--- chapter1.php
+++ chapter1.php
@@ -9,9 +9,23 @@
 //Inside HTML Tags
 
 echo "Hello, World!" ."<br>";
-echo "For Merging lol" ."<br>";
+echo "For Merging lol" ."<br>" ."<br>";
+?>
 
+
+<?php 
+echo "Sample Forms" ."<br>";
+if(!empty($_POST['name'])) {
+echo "GREETINGS, {$_POST['name']}, and Welcome.";
+}
 ?> 
+
+<form action="<?php echo $_SERVER['PHP SELF']; ?>" method="post">
+Enter Your Name: <input type="text" name="name" />
+<input type ="submit" />
+</form>
+
+
 
 </body>
 </html>

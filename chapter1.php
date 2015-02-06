98a852249211421ae4d70be03098c86a9b34644a
mara <maragatchalian@github.com>
2015-02-06 14:13:56 +0800

    Databases. Refer to Ch.1 of Summary Report for more info

--- chapter1.php
+++ chapter1.php
@@ -13,8 +13,9 @@ echo "For Merging lol" ."<br>" ."<br>";
 ?>
 
 
-<?php 
-echo "Sample Forms" ."<br>";
+<?php
+//Forms 
+/*echo "Sample Forms" ."<br>";
 if(!empty($_POST['name'])) {
 echo "GREETINGS, {$_POST['name']}, and Welcome.";
 }
@@ -25,7 +26,42 @@ Enter Your Name: <input type="text" name="name" />
 <input type ="submit" />
 </form>
 
+<?php
+//Databases
+//Using Mysql
+echo "   "; ."<br>" ."<br>";
+echo "Databases"; 
 
+$db = new mysqli("localhost", "root", "klabklab13", "library");
+// make sure the above credentials are correct for your environment
+if ($db->connect_error) {
+die("Connect Error ({$db->connect_errno}) {$db->connect_error}");
+}
+$sql = "SELECT * FROM books WHERE available = 2 ORDER BY title";
+$result = $db->query($sql);
+?>
+<html>
+<body>
+<table cellSpacing="2" cellPadding="6" align="center" border="1">
+<tr>
+<td colspan="4">
+<h3 align="center">These Books are currently available</h3>
+</td>
+</tr>
+<tr>
+<td align="center">Title</td>
+<td align="center">Year Published</td>
+<td align="center">ISBN</td>
+</tr>
+<?php while ($row = $result->fetch_assoc()) { ?>
+<tr>
+<td><?php echo stripslashes($row['title']); ?></td>
+<td align="center"><?php echo $row['pub_year']; ?></td>
+<td><?php echo $row['ISBN']; ?></td>
+</tr>
+<?php } ?>
+</table>
+*/
 
 </body>
 </html>

2397040a9c6e29596c38f4339252a2366a7d331e
mara <maragatchalian@github.com>
2015-02-09 15:24:11 +0800

    Sample Functions andString Concatnation

--- chapter3.php
+++ chapter3.php
@@ -1,7 +1,42 @@
 <?php
 //Strlen function
+
 echo "strlen() function <br>";
 $length = strlen(PHP);
-echo "PHP has $length characters <br>";
+echo "PHP has $length characters <br> <br>";
 
-?> 
+?>
+
+<?php
+function familyname($fname) {
+	echo "$fname Perenal.<br>";
+}
+
+echo "More on Functions: <br>";
+familyname("Eric Jan Dale");
+familyname("Mara");
+familyname("JJ");
+?>
+
+<?php
+//another example of function
+function love($addiction) {
+	echo "$addiction is addicting <br>";
+	}
+love("candy");
+love("chocolate");
+
+?>
+
+<?php
+//String concatination
+echo "  <br> <br> ";
+echo "String Concatenation <br>";
+function strcat($left, $right){
+	return $left . $right;
+	}
+
+$first = "This is a ";
+$second = " complete sentence.";
+
+echo strcat($first, $second);

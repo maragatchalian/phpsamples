f87f9504c8de76531e86409573281cf18ab3e0f7
mara <maragatchalian@github.com>
2015-02-09 12:25:34 +0800

    Infinite Loop

--- flowcontrol.php
+++ flowcontrol.php
@@ -1,18 +1,9 @@
 <?php
-//While
+//While Infinite Loop
 
-echo "While <br>";
-$counter = 0;
-while ($counter < 10){
-echo "Counter is {$counter} <br>";
-$counter++;
-}
-
-echo " <br> For <br>";
-for ($counter = 0; $counter < 10; $counter++){
-	echo "Counter is $counter <br>";
-}
-
+while(1){
+	print "try to stop meeeee <br>";
+	}
 
 ?>  
 

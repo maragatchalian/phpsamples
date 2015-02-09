a6bbfa6b3a563891d18fba38e64f55234f784c56
mara <maragatchalian@github.com>
2015-02-09 12:03:29 +0800

    While loop

--- flowcontrol.php
+++ flowcontrol.php
@@ -1,19 +1,10 @@
 <?php
-//switch
+//While
 
-switch ($favcolor) {
-    case "red":
-        echo "Your favorite color is red!";
-        break;
-    case "blue":
-        echo "Your favorite color is blue!";
-        break;
-    case "green":
-        echo "Your favorite color is green!";
-        break;
-    default:
-        echo "Your favorite color is neither red, blue, or green!";
+$x = 1;
+while($x <= 5) {
+   echo "The number is: $x <br>";
+   $x++;
 }
-
-?>
+?>  
 

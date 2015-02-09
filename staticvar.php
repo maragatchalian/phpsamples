d448f32df1b1d7dd7a4f000c9e9a22099866b914
mara <maragatchalian@github.com>
2015-02-09 11:10:26 +0800

    Static Variables

---
+++ staticvar.php
@@ -0,0 +1,12 @@
+<?php
+function updateCounter(){
+static $counter = 0;
+$counter++;
+echo "Static counter is now {$counter}" ."<br>";
+}
+
+$counter = 10;
+updateCounter();
+updateCounter();
+echo "Global counter is {$counter}" ."<br>";
+?>
